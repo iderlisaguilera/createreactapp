a993ad1610c85b6f5a172a9b1cf529c144fa2ead
Derlis <aguileraderlis8@gmail.com>
2023-01-13 21:24:41 -0300

    respuesta makeitreal2

--- src/App.js
+++ src/App.js
@@ -4,12 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <h1>Mi primera aplicación</h1>
+      <p>Esta es mi primera aplicación en React, está padrísimo!</p>
     </div>
   );
 }
